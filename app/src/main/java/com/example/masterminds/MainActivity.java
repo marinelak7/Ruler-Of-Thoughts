e578e4c8959515fae82f46747eb2c53dd3310881
Marinela Kalthi <marinelak@csd.auth.gr>
2023-05-11 01:41:22 +0300

    Dark blue buttons on all activities , ActionBar on Menu (?) , Quit button on Menu exits the app.

--- app/src/main/java/com/example/masterminds/MainActivity.java
+++ app/src/main/java/com/example/masterminds/MainActivity.java
@@ -5,13 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button quitbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        quitbtn = findViewById(R.id.quitbtn);
+        quitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
     }
 
 
@@ -19,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
 
         Intent i = new Intent(this, Statistics.class);
         startActivity(i);
-
     }
+
+
+
+
+
 }
--- app/src/main/java/com/example/masterminds/Menu.java
+++ app/src/main/java/com/example/masterminds/Menu.java
@@ -7,18 +7,31 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 import java.util.Objects;
 
 public class Menu extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.black)));
+        // Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.dark_blue)));
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+
     }
 
+
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
 
 
 }
