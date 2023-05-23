5e88f4a542acc1396fb2dbfe401199fa0905645b
Themistoklis Androniadis <athemimst@csd.auth.gr>
2023-05-23 18:00:00 +0300

    Normal Difficulty completed.

--- app/src/main/java/com/example/masterminds/GameActivityWithSpinner.java
+++ app/src/main/java/com/example/masterminds/GameActivityWithSpinner.java
@@ -3,73 +3,117 @@ package com.example.masterminds;
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
-import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
-import androidx.core.content.ContextCompat;
+import android.widget.Toast;
 
-import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
-import nl.dionsegijn.konfetti.core.Angle;
-import nl.dionsegijn.konfetti.core.Party;
-import nl.dionsegijn.konfetti.core.PartyFactory;
-import nl.dionsegijn.konfetti.core.Position;
-import nl.dionsegijn.konfetti.core.Spread;
-import nl.dionsegijn.konfetti.core.emitter.Emitter;
-import nl.dionsegijn.konfetti.core.emitter.EmitterConfig;
-//import nl.dionsegijn.konfetti.core.models.Shape;
-import nl.dionsegijn.konfetti.core.models.Size;
+
+import nl.dionsegijn.konfetti.core.models.Shape;
+
 import nl.dionsegijn.konfetti.xml.KonfettiView;
 
 public class GameActivityWithSpinner extends AppCompatActivity {
 
-    FloatingActionButton actionButton;
 
 
-    private KonfettiView konfettiView = null;
-    //private Shape.DrawableShape drawableShape = null;
+
+
     TextView tvt;
-    Pegs [][] pos = new Pegs[2][4];
+    Pegs [][] pos = new Pegs[9][4];
     String []hidden_code = new String[4] ;
     String []users_answer = new String[4];
-    int [] correct_color_wrong_position = new int[2];
-    int [] correct_color_correct_position = new int[2];
+    int [] correct_color_wrong_position = new int[9];
+    int [] correct_color_correct_position = new int[9];
+
+    int [] checkAnswers = new int[9];
     HashMap<String, Integer> unique_answers;
     HashMap<String, Integer> unique_codes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
-        pos[0][0] = new Pegs(R.id.position_29);
-        pos[0][1] = new Pegs(R.id.position_30);
-        pos[0][2] = new Pegs(R.id.position_31);
-        pos[0][3] = new Pegs(R.id.position_32);
-        pos[1][0] = new Pegs(R.id.position_33);
-        pos[1][1] = new Pegs(R.id.position_34);
-        pos[1][2] = new Pegs(R.id.position_35);
-        pos[1][3] = new Pegs(R.id.position_36);
-        unique_codes = new HashMap<>();
-        hidden_code[0] = "Red";
-        hidden_code[1] = "Blue";
-        hidden_code[2] = "Green";
-        hidden_code[3] = "Gray";
-        unique_answers = new HashMap<>();
-        correct_color_correct_position[0] = R.id.red_index_1;
-        correct_color_correct_position[1] = R.id.red_index_2;
 
-        correct_color_wrong_position[0] = R.id.white_index_1;
-        correct_color_wrong_position[1] = R.id.white_index_2;
+
+        if (Pegs.getDifficulty() == 0)
+        {
+            setContentView(R.layout.activity_game);
+        }
+        else if (Pegs.getDifficulty() == 1)
+        {
+            setContentView(R.layout.activity_game);
+        }
+        else
+        {
+            setContentView(R.layout.hard_difficulty);
+        }
+
+        pos[0][0] = new Pegs(R.id.position__1); pos[0][1] = new Pegs(R.id.position__2);
+        pos[0][2] = new Pegs(R.id.position__3); pos[0][3] = new Pegs(R.id.position__4);
+
+        pos[1][0] = new Pegs(R.id.position__5); pos[1][1] = new Pegs(R.id.position__6);
+        pos[1][2] = new Pegs(R.id.position__7); pos[1][3] = new Pegs(R.id.position__8);
+
+        pos[2][0] = new Pegs(R.id.position__9); pos[2][1] = new Pegs(R.id.position__10);
+        pos[2][2] = new Pegs(R.id.position__11); pos[2][3] = new Pegs(R.id.position__12);
+
+        pos[3][0] = new Pegs(R.id.position__13); pos[3][1] = new Pegs(R.id.position__14);
+        pos[3][2] = new Pegs(R.id.position__15); pos[3][3] = new Pegs(R.id.position__16);
+
+        pos[4][0] = new Pegs(R.id.position__17); pos[4][1] = new Pegs(R.id.position__18);
+        pos[4][2] = new Pegs(R.id.position__19); pos[4][3] = new Pegs(R.id.position__20);
+
+        pos[5][0] = new Pegs(R.id.position__21); pos[5][1] = new Pegs(R.id.position__22);
+        pos[5][2] = new Pegs(R.id.position__23); pos[5][3] = new Pegs(R.id.position__24);
+
+        pos[6][0] = new Pegs(R.id.position__25); pos[6][1] = new Pegs(R.id.position__26);
+        pos[6][2] = new Pegs(R.id.position__27); pos[6][3] = new Pegs(R.id.position__28);
+
+        pos[7][0] = new Pegs(R.id.position__29); pos[7][1] = new Pegs(R.id.position__30);
+        pos[7][2] = new Pegs(R.id.position__31); pos[7][3] = new Pegs(R.id.position__32);
+
+        pos[8][0] = new Pegs(R.id.position__33); pos[8][1] = new Pegs(R.id.position__34);
+        pos[8][2] = new Pegs(R.id.position__35); pos[8][3] = new Pegs(R.id.position__36);
+
+
+        createCode();
+
+
+        unique_codes = new HashMap<>();
+
+        unique_answers = new HashMap<>();
+
+
+        correct_color_correct_position[0] = R.id.red_index_1; correct_color_correct_position[1] = R.id.red_index_2;
+        correct_color_correct_position[2] = R.id.red_index_3; correct_color_correct_position[3] = R.id.red_index_4;
+        correct_color_correct_position[4] = R.id.red_index_5; correct_color_correct_position[5] = R.id.red_index_6;
+        correct_color_correct_position[6] = R.id.red_index_7; correct_color_correct_position[7] = R.id.red_index_8;
+        correct_color_correct_position[8] = R.id.red_index_9;
+
+
+
+        correct_color_wrong_position[0] = R.id.white_index_1; correct_color_wrong_position[1] = R.id.white_index_2;
+        correct_color_wrong_position[2] = R.id.white_index_3; correct_color_wrong_position[3] = R.id.white_index_4;
+        correct_color_wrong_position[4] = R.id.white_index_5; correct_color_wrong_position[5] = R.id.white_index_6;
+        correct_color_wrong_position[6] = R.id.white_index_7; correct_color_wrong_position[7] = R.id.white_index_8;
+        correct_color_wrong_position[8] = R.id.white_index_9;
+
+        checkAnswers[0] = R.id.checkAnswer_1; checkAnswers[1] = R.id.checkAnswer_2;
+        checkAnswers[2] = R.id.checkAnswer_3; checkAnswers[3] = R.id.checkAnswer_4;
+        checkAnswers[4] = R.id.checkAnswer_5; checkAnswers[5] = R.id.checkAnswer_6;
+        checkAnswers[6] = R.id.checkAnswer_7; checkAnswers[7] = R.id.checkAnswer_8;
+        checkAnswers[8] = R.id.checkAnswer_9;
 
         for (int i = 0; i < 4; i++)
         {
@@ -82,8 +126,7 @@ public class GameActivityWithSpinner extends AppCompatActivity {
         }
         //Toast toast = Toast.makeText(getApplicationContext(), "HELLO", Toast.LENGTH_SHORT);
         //toast.show();
-        tvt = findViewById(R.id.numberOfTurns);
-        tvt.setText("Turn 1");
+
     }
     int current_turn = 1;
     int turns = 9;
@@ -95,7 +138,7 @@ public class GameActivityWithSpinner extends AppCompatActivity {
 
        // Toast toast = Toast.makeText(GameActivityWithSpinner.this, "Hey there", Toast.LENGTH_SHORT);
        // toast.show();
-        
+
         Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.color_choice_spinner);
         tvt = findViewById(R.id.textView5);
@@ -215,16 +258,16 @@ public class GameActivityWithSpinner extends AppCompatActivity {
                 break;
             }
         }
-        FloatingActionButton choose = findViewById(R.id.checkAnswer);
+        FloatingActionButton choose = findViewById(checkAnswers[current_turn - 1]);
         if (flag == 1)
         {
             choose.setVisibility(View.VISIBLE);
-            tvt.setText("No clear positions.");
+            //tvt.setText("No clear positions.");
         }
         else
         {
             choose.setVisibility(View.INVISIBLE);
-            tvt.setText("Some positions are clear.");
+            //tvt.setText("Some positions are clear.");
         }
 
         return false;
@@ -269,7 +312,7 @@ public class GameActivityWithSpinner extends AppCompatActivity {
                 }
             }
         }
-        tvt = findViewById(R.id.numberOfTurns);
+
         for (String item : copy_code.keySet())
         {
             if (copy_code.get(item) != 0)
@@ -377,13 +420,57 @@ public class GameActivityWithSpinner extends AppCompatActivity {
         tvt = findViewById(correct_color_wrong_position[current_turn - 1]);
         tvt.setText(Integer.toString(white_index));
 
-        current_turn++;
-        FloatingActionButton check = findViewById(R.id.checkAnswer);
-        tvt = findViewById(R.id.numberOfTurns);
-        tvt.setText("Turn " + current_turn);
+
+        FloatingActionButton check = findViewById(checkAnswers[current_turn - 1]);
+
         check.setVisibility(View.INVISIBLE);
 
+        current_turn++;
+
+        if (current_turn > turns)
+        {
+            Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.results_loss);
+
+            Button ok = dialog.findViewById(R.id.ok_button_loss);
+            Intent i = new Intent(this, ChooseDifficultyMenuVer1.class);
+            ok.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(i);
+                }
+            });
+
+
+            dialog.show();
+        }
 
         unique_answers.clear();
+    }
+
+
+    void createCode()
+    {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Red"); list.add("Blue");
+        list.add("Yellow"); list.add("Green");
+        list.add("Cyan"); list.add("White");
+        list.add("Magenta"); list.add("Gray");
+
+        Collections.shuffle(list);
+
+        for (int i = 0; i < 4; i++)
+        {
+            hidden_code[i] = list.get(i);
+        }
+
+
+    }
+
+    public void showAnswer(View view)
+    {
+        Toast.makeText(this, hidden_code[0] + " " + hidden_code[1] + " " +
+                hidden_code[2] + " " + hidden_code[3], Toast.LENGTH_LONG).show();
     }
 }
